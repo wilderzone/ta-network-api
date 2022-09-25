8c41add9420b9bec7c0da5e28c6609987ab5913a
Giga <gigabyte5671@gmail.com>
2022-09-25 14:21:55 +1300

    Create menu_options.py

---
+++ data/menu_options.py
@@ -0,0 +1,16 @@
+MENU_OPTIONS = {
+	'6A040000': 'CONTROL - Ski',
+	'6B040000': 'CONTROL - Jump',
+	'6C040000': 'CONTROL - Jetpack',
+	'6D040000': 'CONTROL - Jump/Jetpack',
+	'70040000': 'CONTROL - Zoom',
+	'71040000': 'CONTROL - Fire',
+	'74040000': 'CONTROL - Interact',
+	'75040000': 'CONTROL - Melee',
+	'76040000': 'CONTROL - Use Pack',
+	'77040000': 'CONTROL - Drop Flag',
+	'78040000': 'CONTROL - Begin VGS',
+	'7D040000': 'CONTROL - Spot Target',
+	'81040000': 'CONTROL - Third-Person Cam',
+	'8F040000': 'CONTROL - Chat Console'
+}
