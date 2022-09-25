108960142f42d3d07a421ed40619588a898e84ce
Giga <gigabyte5671@gmail.com>
2022-09-26 12:15:26 +1300

    Create Data interface

---
+++ interfaces/Data.ts
@@ -0,0 +1,22 @@
+export interface HiRezAccount {
+	version: Uint8Array
+	salt: Uint8Array,
+	username: string,
+	playername: string,
+	clanTag: string,
+	rank: number,
+	rankProgress: number,
+	xp: number,
+	gold: number,
+	region: Uint8Array
+}
+
+export interface HiRezCustomGameServer {
+	name: string,
+	description: string,
+	password: string,
+	rankLimit?: {
+		lower: number,
+		upper: number
+	}
+}
