e76c3080976171656470d5efa6d28e0a3243a1ea
Giga <gigabyte5671@gmail.com>
2022-09-26 12:33:48 +1300

    Add GameServer interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,4 +1,4 @@
 export { Credentials, HashedCredentials } from './Credentials';
 export { HiRezAccount, HiRezCustomGameServer } from './Data';
 export { Map } from './Maps';
-export { Server } from './Servers';
+export { LoginServer, GameServer } from './Servers';
