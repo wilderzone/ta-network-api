c61df5fc582030410f670b40b5cb383aa1b719b9
Giga <gigabyte5671@gmail.com>
2022-09-26 12:16:21 +1300

    Add Data interfaces to module

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,3 +1,4 @@
 export { Credentials, HashedCredentials } from './Credentials';
+export { HiRezAccount, HiRezCustomGameServer } from './Data';
 export { Map } from './Maps';
 export { Server } from './Servers';
