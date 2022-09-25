9127674056c88d174b046c35adf838aa87deda0c
Giga <gigabyte5671@gmail.com>
2022-09-25 21:27:05 +1300

    Create Maps data

---
+++ data/Maps.ts
@@ -0,0 +1,400 @@
+import { Map } from '../interfaces';
+
+export const Maps = {
+	ctf_katabatic: {
+	  id: 1447,
+	  name: 'Katabatic',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Katabatic'
+	},
+	ctf_arx_novena: {
+	  id: 1456,
+	  name: 'Arx Novena',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.ArxNovena'
+	},
+	ctf_dangerous_crossing: {
+	  id: 1538,
+	  name: 'Dangerous Crossing',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.DangerousCrossing'
+	},
+	ctf_crossfire: {
+	  id: 1462,
+	  name: 'Crossfire',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Crossfire'
+	},
+	ctf_drydock: {
+	  id: 1457,
+	  name: 'Drydock',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Drydock'
+	},
+	ctf_terminus: {
+	  'key': 'ctf_terminus',
+	  id: 1553,
+	  name: 'Terminus',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Terminus'
+	},
+	ctf_sunstar: {
+	  'key': 'ctf_sunstar',
+	  id: 1523,
+	  name: 'Sunstar',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Sunstar'
+	},
+	ctf_bella_omega: {
+	  id: 1473,
+	  name: 'Bella Omega',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.BellaOmega'
+	},
+	ctf_bella_omega_ns: {
+	  id: 1551,
+	  name: 'Bella Omega NS',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.BellaOmegaNS'
+	},
+	ctf_blueshift: {
+	  id: 1543,
+	  name: 'Blueshift',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Blueshift'
+	},
+	ctf_canyon_crusade: {
+	  id: 1514,
+	  name: 'Canyon Crusade',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.CanyonCrusade'
+	},
+	ctf_hellfire: {
+	  id: 1560,
+	  name: 'Hellfire',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Hellfire'
+	},
+	ctf_icecoaster: {
+	  id: 1554,
+	  name: 'IceCoaster',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.IceCoaster'
+	},
+	ctf_perdition: {
+	  id: 1555,
+	  name: 'Perdition',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Perdition'
+	},
+	ctf_permafrost: {
+	  id: 1534,
+	  name: 'Permafrost',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Permafrost'
+	},
+	ctf_raindance: {
+	  id: 1516,
+	  name: 'Raindance',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Raindance'
+	},
+	ctf_stonehenge: {
+	  id: 1522,
+	  name: 'Stonehenge',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Stonehenge'
+	},
+	ctf_tartarus: {
+	  id: 1512,
+	  name: 'Tartarus',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.Tartarus'
+	},
+	ctf_temple_ruins: {
+	  id: 1493,
+	  name: 'Temple Ruins',
+	  gamemode: 'CTF',
+	  lua: 'Maps.CTF.TempleRuins'
+	},
+	ctf_blues: {
+	  id: 1562,
+	  name: 'Blues',
+	  gamemode: 'CTF',
+	  lua: 'TrCTF-Blues',
+	  isCustom: true
+	},
+	ctf_incidamus: {
+	  id: 1563,
+	  name: 'Incidamus',
+	  gamemode: 'CTF',
+	  lua: 'TrCTF-Incidamus',
+	  isCustom: true
+	},
+	ctf_periculo: {
+	  id: 1564,
+	  name: 'Periculo',
+	  gamemode: 'CTF',
+	  lua: 'TrCTF-Periculo',
+	  isCustom: true
+	},
+	ctf_fracture: {
+	  id: 1565,
+	  name: 'Fracture',
+	  gamemode: 'CTF',
+	  lua: 'TrCTF-Fracture',
+	  isCustom: true
+	},
+	ctf_phlegethon: {
+	  id: 1566,
+	  name: 'Phlegethon',
+	  gamemode: 'CTF',
+	  lua: 'TrCTF-Phlegethon',
+	  isCustom: true
+	},
+	ctf_deserted_valley: {
+	  id: 1567,
+	  name: 'Deserted Valley',
+	  gamemode: 'CTF',
+	  lua: 'TrCTF-DesertedValley',
+	  isCustom: true
+	},
+	ctf_acheron: {
+	  id: 1568,
+	  name: 'Acheron',
+	  gamemode: 'CTF',
+	  lua: 'TrCTF-Acheron',
+	  isCustom: true
+	},
+	ctf_styx: {
+	  id: 1569,
+	  name: 'Styx',
+	  gamemode: 'CTF',
+	  lua: 'TrCTF-Styx',
+	  isCustom: true
+	},
+	arena_walledin: {
+	  id: 1497,
+	  name: 'Walled In',
+	  gamemode: 'Arena',
+	  lua: 'Maps.Arena.WalledIn'
+	},
+	arena_hinterlands: {
+	  id: 1533,
+	  name: 'Hinterlands',
+	  gamemode: 'Arena',
+	  lua: 'Maps.Arena.Hinterlands'
+	},
+	arena_fraytown: {
+	  id: 1545,
+	  name: 'Fray Town',
+	  gamemode: 'Arena',
+	  lua: 'Maps.Arena.FrayTown'
+	},
+	arena_undercroft: {
+	  id: 1546,
+	  name: 'Undercroft',
+	  gamemode: 'Arena',
+	  lua: 'Maps.Arena.Undercroft'
+	},
+	arena_airarena: {
+	  id: 1495,
+	  name: 'Air Arena',
+	  gamemode: 'Arena',
+	  lua: 'Maps.Arena.AirArena'
+	},
+	arena_lavaarena: {
+	  id: 1498,
+	  name: 'Lava Arena',
+	  gamemode: 'Arena',
+	  lua: 'Maps.Arena.LavaArena'
+	},
+	arena_whiteout: {
+	  id: 1544,
+	  name: 'Whiteout',
+	  gamemode: 'Arena',
+	  lua: 'Maps.Arena.Whiteout'
+	},
+	arena_elysian_battleground: {
+	  id: 1570,
+	  name: 'Elysian Battleground',
+	  gamemode: 'Arena',
+	  lua: 'TrArena-ElysianBattleground',
+	  isCustom: true
+	},
+	rabbit_nightabatic: {
+	  id: 1494,
+	  name: 'Nightabatic',
+	  gamemode: 'Rabbit',
+	  lua: 'Maps.Rabbit.Nightabatic'
+	},
+	rabbit_sulfurcove: {
+	  id: 1496,
+	  name: 'Sulfur Cove',
+	  gamemode: 'Rabbit',
+	  lua: 'Maps.Rabbit.SulfurCove'
+	},
+	rabbit_inferno: {
+	  id: 1491,
+	  name: 'Inferno',
+	  gamemode: 'Rabbit',
+	  lua: 'Maps.Rabbit.Inferno'
+	},
+	rabbit_crossfire: {
+	  id: 1464,
+	  name: 'Crossfire',
+	  gamemode: 'Rabbit',
+	  lua: 'Maps.Rabbit.Crossfire'
+	},
+	rabbit_outskirts: {
+	  id: 1458,
+	  name: 'Outskirts',
+	  gamemode: 'Rabbit',
+	  lua: 'Maps.Rabbit.Outskirts'
+	},
+	rabbit_quicksand: {
+	  id: 1461,
+	  name: 'Quicksand',
+	  gamemode: 'Rabbit',
+	  lua: 'Maps.Rabbit.Quicksand'
+	},
+	tdm_outskirts: {
+	  id: 1490,
+	  name: 'Outskirts',
+	  gamemode: 'TDM',
+	  lua: 'Maps.TDM.Outskirts'
+	},
+	tdm_quicksand: {
+	  id: 1484,
+	  name: 'Quicksand',
+	  gamemode: 'TDM',
+	  lua: 'Maps.TDM.Quicksand'
+	},
+	tdm_sulfur_cove: {
+	  id: 1488,
+	  name: 'Sulfur Cove',
+	  gamemode: 'TDM',
+	  lua: 'Maps.TDM.SulfurCove'
+	},
+	tdm_drydock_night: {
+	  id: 1480,
+	  name: 'DrydockNight',
+	  gamemode: 'TDM',
+	  lua: 'Maps.TDM.DrydockNight'
+	},
+	tdm_nightabatic: {
+	  id: 1485,
+	  name: 'Nightabatic',
+	  gamemode: 'TDM',
+	  lua: 'Maps.TDM.Nightabatic'
+	},
+	tdm_inferno: {
+	  id: 1487,
+	  name: 'Inferno',
+	  gamemode: 'TDM',
+	  lua: 'Maps.TDM.Inferno'
+	},
+	tdm_miasma: {
+	  id: 1536,
+	  name: 'Miasma',
+	  gamemode: 'TDM',
+	  lua: 'Maps.TDM.Miasma'
+	},
+	blitz_arx_novena: {
+	  id: 1540,
+	  name: 'ArxNovena',
+	  gamemode: 'Blitz',
+	  lua: 'Maps.Blitz.ArxNovena'
+	},
+	blitz_bella_omega: {
+	  id: 1550,
+	  name: 'BellaOmega',
+	  gamemode: 'Blitz',
+	  lua: 'Maps.Blitz.BellaOmega'
+	},
+	blitz_blueshift: {
+	  id: 1552,
+	  name: 'Blueshift',
+	  gamemode: 'Blitz',
+	  lua: 'Maps.Blitz.Blueshift'
+	},
+	blitz_crossfire: {
+	  id: 1542,
+	  name: 'Crossfire',
+	  gamemode: 'Blitz',
+	  lua: 'Maps.Blitz.Crossfire'
+	},
+	blitz_canyon: {
+	  id: 1549,
+	  name: 'Canyon Crusade Revival',
+	  gamemode: 'Blitz',
+	  lua: 'Maps.Blitz.CanyonCrusadeRevival'
+	},
+	blitz_drydock: {
+	  id: 1541,
+	  name: 'Drydock',
+	  gamemode: 'Blitz',
+	  lua: 'Maps.Blitz.Drydock'
+	},
+	blitz_hellfire: {
+	  id: 1561,
+	  name: 'Hellfire',
+	  gamemode: 'Blitz',
+	  lua: 'Maps.Blitz.Hellfire'
+	},
+	blitz_icecoaster: {
+	  id: 1558,
+	  name: 'IceCoaster',
+	  gamemode: 'Blitz',
+	  lua: 'Maps.Blitz.IceCoaster'
+	},
+	blitz_katabatic: {
+	  id: 1539,
+	  name: 'Katabatic',
+	  gamemode: 'Blitz',
+	  lua: 'Maps.Blitz.Katabatic'
+	},
+	blitz_broadside: {
+	  id: 1542,
+	  name: 'Broadside',
+	  gamemode: 'Blitz',
+	  lua: 'TrCTFBlitz-Broadside',
+	  isCustom: true
+	},
+	cah_outskirts: {
+	  id: 1526,
+	  name: 'Outskirts',
+	  gamemode: 'CaH',
+	  lua: 'Maps.CaH.Outskirts'
+	},
+	cah_katabatic: {
+	  id: 1521,
+	  name: 'Katabatic',
+	  gamemode: 'CaH',
+	  lua: 'Maps.CaH.Katabatic'
+	},
+	cah_raindance: {
+	  id: 1528,
+	  name: 'Raindance',
+	  gamemode: 'CaH',
+	  lua: 'Maps.CaH.Raindance'
+	},
+	cah_sulfur_cove: {
+	  id: 1535,
+	  name: 'Sulfur Cove',
+	  gamemode: 'CaH',
+	  lua: 'Maps.CaH.SulfurCove'
+	},
+	cah_tartarus: {
+	  id: 1537,
+	  name: 'Tartarus',
+	  gamemode: 'CaH',
+	  lua: 'Maps.CaH.Tartarus'
+	},
+	cah_canyon: {
+	  id: 1548,
+	  name: 'Canyon Crusade Revival',
+	  gamemode: 'CaH',
+	  lua: 'Maps.CaH.CanyonCrusadeRevival'
+	}
+} as { [key: string]: Map };
