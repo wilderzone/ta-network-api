8031510615a0e143c2e731274358f8c86f23b57f
Giga <gigabyte5671@gmail.com>
2022-09-26 21:31:31 +1300

    Fix password-XOR functions
    - Fix TS functions for XOR'ing passwords.
    - Remove equivalent Python file (`XOR.py`).

--- functions/XOR.ts
+++ functions/XOR.ts
@@ -1,4 +1,32 @@
-export function passwordHashXOR (passwordHash: Uint8Array, salt: Uint8Array): Uint8Array {
+/**
+ * An iterator that aggregates elements from each of the iterables. If the iterables are of uneven length, missing values are filled-in with `fillValue`. Iteration continues until the longest iterable is exhausted.
+ * 
+ * From: https://docs.python.org/3/library/itertools.html#itertools.zip_longest
+ * 
+ * @param iterables An array of Uint8Arrays to zip.
+ * @param fillValue Value to use in place of empty elements. 
+ * @returns The aggregated (zipped) array.
+ */
+function zipLongest(iterables: Uint8Array[], fillValue = 0): Uint8Array[] {
+	const longest = Math.max(...(iterables.map(el => el.length)));
+	let zippedArray = [] as Uint8Array[];
+	for (let i = 0; i < longest; i++) {
+		let zippedBlock = new Uint8Array(iterables.length);
+		iterables.forEach((value, index) => {
+			zippedBlock[index] = value[i] ?? fillValue;
+		});
+		zippedArray[i] = zippedBlock;
+	}
+	return zippedArray;
+}
+
+/**
+ * Perform an XOR operation on a hashed password and a salt value.
+ * @param passwordHash 
+ * @param salt 
+ * @returns The XOR'ed value. Suitable for Login Server authentication.
+ */
+export function xorPasswordHash (passwordHash: Uint8Array, salt: Uint8Array): Uint8Array {
 	// Split the salt into groups of 4 bits (nibbles).
 	let saltNibbles = [] as number[];
 	salt.forEach((value) => {
@@ -13,7 +41,7 @@ export function passwordHashXOR (passwordHash: Uint8Array, salt: Uint8Array): Ui
 	});
 
 	// Create the XOR pattern from the list of values.
-	const pattern = [
+	const pattern = new Uint8Array([
 		xorValues[6], 0,
 		xorValues[7], 0,
 		xorValues[4], 0,
@@ -50,19 +78,23 @@ export function passwordHashXOR (passwordHash: Uint8Array, salt: Uint8Array): Ui
 		xorValues[29], 0,
 		xorValues[30], 0,
 		xorValues[31], 0,
-	];
+	]);
 
 	// Execute the XOR pattern on the password hash.
-	
-	// This needs further analysis, as the conversion from Python to JS is not straightforward.
+	const zip = zipLongest([passwordHash, pattern], 0);
+	const processedPasswordHash = new Uint8Array(zip.length);
 
-	/* const temp = itertools.zip_longest(passwordHash, pattern, fillvalue = 0);
-	const processedPasswordHash = [];
-	for (const [p, x] in temp) {
-		processedPasswordHash.push(p ^ x);
-	}
+	zip.forEach((zippedBlock, index) => {
+		let result = undefined as number | undefined;
+		zippedBlock.forEach((value) => {
+			if (typeof result === 'undefined') {
+				result = value;
+			} else {
+				result = result ^ value;
+			}
+		});
+		processedPasswordHash[index] = result ?? 0;
+	});
 
-	return processedPasswordHash; */
-
-	return new Uint8Array;
+	return processedPasswordHash;
 }
