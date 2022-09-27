f20cc2566d47c154e0d6475a570e50fe1312539b
Giga <gigabyte5671@gmail.com>
2022-09-28 11:41:48 +1300

    Add generic `invertEndianness` function

--- functions/Utils.ts
+++ functions/Utils.ts
@@ -14,3 +14,20 @@ export function textToHexInt (string: string): number[] {
 	const splitString = string.split('');
 	return splitString.map((char) => char.charCodeAt(0));
 }
+
+/**
+ * Inverts the endianness of a Uint8Array buffer.
+ * @param buffer The buffer to invert.
+ * @returns The now inverted buffer.
+ */
+export function invertEndianness (buffer: Uint8Array): Uint8Array {
+	if (buffer.length % 2 !== 0) {
+		buffer = new Uint8Array([...buffer, 0]);
+	}
+	let invertedBuffer = new Uint8Array(buffer.length);
+	for (let i = 0; i < buffer.length; i += 2) {
+		invertedBuffer[i] = buffer[i + 1];
+		invertedBuffer[i + 1] = buffer[i];
+	}
+	return invertedBuffer;
+}
