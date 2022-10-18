a69f2d3a934e441334d3e0f506f5be6bc61b2225
Giga <gigabyte5671@gmail.com>
2022-10-18 21:57:10 +1300

    Add Regions data

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,4 @@
 export { generalEnumfields, serverListEnumfields, versionEnumFields } from './Enumfields';
 export { Maps } from './Maps';
+export { Regions } from './Regions';
 export { loginServers } from './Servers';
