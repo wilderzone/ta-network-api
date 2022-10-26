ed419b779ce38f91713e8b7b120f32293ee46f35
Giga <gigabyte5671@gmail.com>
2022-10-26 15:59:16 +1300

    Create game items data

---
+++ data/Items.ts
@@ -0,0 +1,235 @@
+export const Items = {
+	// Light Impact Weapons.
+	'7422': {
+		key: 'Pathfinder_Primary_LightSpinfusor',
+		name: 'Light Spinfusor',
+		class: 'light',
+		type: 'weapon',
+		category: 'impact'
+	},
+	'7425': {
+		key: 'Pathfinder_Primary_BoltLauncher',
+		name: 'Bolt Launcher',
+		class: 'light',
+		type: 'weapon',
+		category: 'impact'
+	},
+	'8696': {
+		key: 'Pathfinder_Primary_LightSpinfusor_100X',
+		name: 'Blinksfusor',
+		class: 'light',
+		type: 'weapon',
+		category: 'impact'
+	},
+	'8245': {
+		key: 'Pathfinder_Primary_LightTwinfusor',
+		name: 'Light Twinfusor',
+		class: 'light',
+		type: 'weapon',
+		category: 'impact'
+	},
+	// Light Timed Weapons.
+	'8761': {
+		key: 'Light_Primary_LightGrenadeLauncher',
+		name: 'Light Grenade Launcher',
+		class: 'light',
+		type: 'weapon',
+		category: 'timed'
+	},
+	'8252': {
+		key: 'Infiltrator_Primary_RemoteArxBuster',
+		name: 'Jackal',
+		class: 'light',
+		type: 'weapon',
+		category: 'timed'
+	},
+	// Light Speciality Weapons.
+	'7400': {
+		key: 'Sentinel_Primary_SniperRifle',
+		name: 'BXT-1',
+		class: 'light',
+		type: 'weapon',
+		category: 'speciality'
+	},
+	'7395': {
+		key: 'Sentinel_Primary_PhaseRifle',
+		name: 'Phase Rifle',
+		class: 'light',
+		type: 'weapon',
+		category: 'speciality'
+	},
+	// Light Bullet Weapons.
+	'7438': {
+		key: 'Pathfinder_Secondary_LightAssaultRifle',
+		name: 'Light Assault Rifle',
+		class: 'light',
+		type: 'weapon',
+		category: 'bullet'
+	},
+	'7419': {
+		key: 'Sentinel_Secondary_Falcon',
+		name: 'Falcon',
+		class: 'light',
+		type: 'weapon',
+		category: 'bullet'
+	},
+	'7433': {
+		key: 'Light_Sidearm_Sparrow',
+		name: 'Sparrow',
+		class: 'light',
+		type: 'weapon',
+		category: 'bullet'
+	},
+	'8256': {
+		key: 'Infiltrator_Secondary_ThrowingKnives',
+		name: 'Throwing Knives',
+		class: 'light',
+		type: 'weapon',
+		category: 'bullet'
+	},
+	// Light Short-Range Weapons.
+	'7399': {
+		key: 'Pathfinder_Secondary_Shotgun',
+		name: 'Shotgun',
+		class: 'light',
+		type: 'weapon',
+		category: 'short_range'
+	},
+	'7435': {
+		key: 'All_H1_Shocklance',
+		name: 'Shocklance',
+		class: 'light',
+		type: 'weapon',
+		category: 'short_range'
+	},
+	// Light Belt Items.
+	'7387': {
+		key: 'Pathfinder_Belt_ImpactNitron',
+		name: 'Impact Nitron',
+		class: 'light',
+		type: 'belt',
+		category: undefined
+	},
+	'7437': {
+		key: 'Pathfinder_Belt_STGrenade',
+		name: 'ST Grenade',
+		class: 'light',
+		type: 'belt',
+		category: undefined
+	},
+	'7914': {
+		key: 'Sentinel_Belt_GrenadeT5',
+		name: 'T5 Grenade',
+		class: 'light',
+		type: 'belt',
+		category: undefined
+	},
+	'7402': {
+		key: 'Infiltrator_Belt_StickyGrenade',
+		name: 'Sticky Grenade',
+		class: 'light',
+		type: 'belt',
+		category: undefined
+	},
+	'7421': {
+		key: 'Sentinel_Belt_Claymore',
+		name: 'Claymore',
+		class: 'light',
+		type: 'belt',
+		category: undefined
+	},
+	'7440': {
+		key: 'Infiltrator_Belt_PrismMines',
+		name: 'Prism Mines',
+		class: 'light',
+		type: 'belt',
+		category: undefined
+	},
+	'8248': {
+		key: 'Infiltrator_Belt_NinjaSmoke',
+		name: 'Ninja Smoke',
+		class: 'light',
+		type: 'belt',
+		category: undefined
+	},
+	// Light Packs.
+	'7822': {
+		key: 'Pathfinder_Pack_JumpPack',
+		name: 'Jump Pack',
+		class: 'light',
+		type: 'pack',
+		category: undefined
+	},
+	'7825': {
+		key: 'Pathfinder_Pack_EnergyRecharge',
+		name: 'Energy Recharge',
+		class: 'light',
+		type: 'pack',
+		category: undefined
+	},
+	'7833': {
+		key: 'Infiltrator_Pack_Stealth',
+		name: 'Stealth',
+		class: 'light',
+		type: 'pack',
+		category: undefined
+	},
+	'7900': {
+		key: 'Sentinel_Pack_EnergyRecharge',
+		name: 'Energy Recharge',
+		class: 'light',
+		type: 'pack',
+		category: undefined
+	},
+	// Light Skins.
+	'7834': {
+		key: 'Skin_PTH',
+		name: 'Pathfinder',
+		class: 'light',
+		type: 'skin',
+		category: undefined
+	},
+	'7835': {
+		key: 'Skin_INF',
+		name: 'Infiltrator',
+		class: 'light',
+		type: 'skin',
+		category: undefined
+	},
+	'8327': {
+		key: 'Skin_SEN',
+		name: 'Sentinel',
+		class: 'light',
+		type: 'skin',
+		category: undefined
+	},
+	'8326': {
+		key: 'Skin_PTH_Mercenary',
+		name: 'Pathfinder Mercenary',
+		class: 'light',
+		type: 'skin',
+		category: undefined
+	},
+	'8336': {
+		key: 'Skin_INF_Mercenary',
+		name: 'Infiltrator Mercenary',
+		class: 'light',
+		type: 'skin',
+		category: undefined
+	},
+	'8337': {
+		key: 'Skin_INF_Assassin',
+		name: 'Infiltrator Assassin',
+		class: 'light',
+		type: 'skin',
+		category: undefined
+	},
+	'8665': {
+		key: 'Skin_SEN_Mercenary',
+		name: 'Sentinel Mercenary',
+		class: 'light',
+		type: 'skin',
+		category: undefined
+	},
+};
+
