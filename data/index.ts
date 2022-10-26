85f194bcd74c66e708c761a555e12ba0cd394ca5
Giga <gigabyte5671@gmail.com>
2022-10-26 15:59:16 +1300

    Create game items data

--- data/index.ts
+++ data/index.ts
@@ -1,4 +1,5 @@
 export { generalEnumfields, serverListEnumfields, versionEnumFields } from './Enumfields';
+export { Items } from './Items';
 export { Maps } from './Maps';
 export { Regions } from './Regions';
 export { loginServers } from './Servers';
