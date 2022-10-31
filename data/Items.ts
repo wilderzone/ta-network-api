d5be92843c3eb77d06c7aabae4cb20d416220862
Giga <gigabyte5671@gmail.com>
2022-11-01 10:06:57 +1300

    Add Medium items data

--- data/Items.ts
+++ data/Items.ts
@@ -231,5 +231,265 @@ export const Items = {
 		type: 'skin',
 		category: undefined
 	},
+	// Medium Impact Weapons.
+	'7401': {
+		key: 'Soldier_Primary_Spinfusor',
+		name: 'Spinfusor',
+		class: 'medium',
+		type: 'waepon',
+		category: 'impact'
+	},
+	'7461': {
+		key: 'Technician_Primary_Thumper',
+		name: 'Thumper',
+		class: 'medium',
+		type: 'waepon',
+		category: 'impact'
+	},
+	'8417': {
+		key: 'Soldier_Secondary_ThumperD_MKD',
+		name: 'Thumper Mk D',
+		class: 'medium',
+		type: 'waepon',
+		category: 'impact'
+	},
+	'8257': {
+		key: 'Soldier_Primary_Twinfusor',
+		name: 'Twinfusor',
+		class: 'medium',
+		type: 'waepon',
+		category: 'impact'
+	},
+	'8697': {
+		key: 'Soldier_Primary_Spinfusor_100X',
+		name: 'Medium Blinksfusor',
+		class: 'medium',
+		type: 'waepon',
+		category: 'impact'
+	},
+	// Medium Timed Weapons.
+	'7384': {
+		key: 'Raider_Primary_ArxBuster',
+		name: 'Arx Buster',
+		class: 'medium',
+		type: 'waepon',
+		category: 'timed'
+	},
+	'7416': {
+		key: 'Raider_Primary_GrenadeLauncher',
+		name: 'Grenade Launcher',
+		class: 'medium',
+		type: 'waepon',
+		category: 'timed'
+	},
+	// Medium Speciality Weapons.
+	'7436': {
+		key: 'Technician_Secondary_RepairToolSD',
+		name: 'Advanced Repair Tool',
+		class: 'medium',
+		type: 'waepon',
+		category: 'speciality'
+	},
+	'8765': {
+		key: 'Medium_ElfProjector',
+		name: 'Elf Projector',
+		class: 'medium',
+		type: 'waepon',
+		category: 'speciality'
+	},
+	'8768': {
+		key: 'Soldier_Primary_Honorfusor',
+		name: 'Honorfusor',
+		class: 'medium',
+		type: 'waepon',
+		category: 'speciality'
+	},
+	// Medium Bullet Weapons.
+	'7385': {
+		key: 'Soldier_Primary_AssaultRifle',
+		name: 'Assault Rifle',
+		class: 'medium',
+		type: 'waepon',
+		category: 'bullet'
+	},
+	'7441': {
+		key: 'Raider_Secondary_NJ4SMG',
+		name: 'NJ4 SMG',
+		class: 'medium',
+		type: 'waepon',
+		category: 'bullet'
+	},
+	'8249': {
+		key: 'Raider_Secondary_NJ5SMG',
+		name: 'NJ5 SMG',
+		class: 'medium',
+		type: 'waepon',
+		category: 'bullet'
+	},
+	'8251': {
+		key: 'Raider_Primary_PlasmaGun',
+		name: 'Plasma Gun',
+		class: 'medium',
+		type: 'waepon',
+		category: 'bullet'
+	},
+	'7394': {
+		key: 'Medium_Sidearm_NovaBlaster',
+		name: 'Nova Blaster',
+		class: 'medium',
+		type: 'waepon',
+		category: 'bullet'
+	},
+	'7388': {
+		key: 'Soldier_Secondary_Eagle',
+		name: 'Eagle',
+		class: 'medium',
+		type: 'waepon',
+		category: 'bullet'
+	},
+	// Medium Short-Range Weapons.
+	'7427': {
+		key: 'Technician_Secondary_SawedOff',
+		name: 'Sawed-Off Shotgun',
+		class: 'medium',
+		type: 'waepon',
+		category: 'short_range'
+	},
+	'8699': {
+		key: 'Technician_Primary_TC24',
+		name: 'TC24',
+		class: 'medium',
+		type: 'waepon',
+		category: 'short_range'
+	},
+	// Medium Belt Items.
+	'7444': {
+		key: 'Raider_Belt_EMPGrenade',
+		name: 'EMP Grenade',
+		class: 'medium',
+		type: 'belt',
+		category: undefined
+	},
+	'7432': {
+		key: 'Raider_Belt_WhiteOut',
+		name: 'White-Out Grenade',
+		class: 'medium',
+		type: 'belt',
+		category: undefined
+	},
+	'8247': {
+		key: 'Raider_Belt_MIRVGrenade',
+		name: 'MIRV Grenade',
+		class: 'medium',
+		type: 'belt',
+		category: undefined
+	},
+	'7434': {
+		key: 'Soldier_Belt_APGrenade',
+		name: 'AP Grenade',
+		class: 'medium',
+		type: 'belt',
+		category: undefined
+	},
+	'7426': {
+		key: 'Technician_Belt_MotionAlarm',
+		name: 'Motion Alarm',
+		class: 'medium',
+		type: 'belt',
+		category: undefined
+	},
+	// Medium Packs.
+	'7832': {
+		key: 'Raider_Pack_Shield',
+		name: 'Shield Pack',
+		class: 'medium',
+		type: 'pack',
+		category: undefined
+	},
+	'7827': {
+		key: 'Raider_Pack_Jammer',
+		name: 'Jammer Pack',
+		class: 'medium',
+		type: 'pack',
+		category: undefined
+	},
+	'8223': {
+		key: 'Soldier_Pack_Utility',
+		name: 'Utility Pack',
+		class: 'medium',
+		type: 'pack',
+		category: undefined
+	},
+	'7413': {
+		key: 'Technician_Pack_LightTurret',
+		name: 'Light Turret',
+		class: 'medium',
+		type: 'pack',
+		category: undefined
+	},
+	'7417': {
+		key: 'Technician_Pack_EXRTurret',
+		name: 'EXR Turret',
+		class: 'medium',
+		type: 'pack',
+		category: undefined
+	},
+	'7456': {
+		key: 'Sentinel_Pack_DropJammer',
+		name: 'Jammer Pack',
+		class: 'medium',
+		type: 'pack',
+		category: undefined
+	},
+	// Medium Skins.
+	'8328': {
+		key: 'Skin_SLD',
+		name: 'Soldier',
+		class: 'medium',
+		type: 'skin',
+		category: undefined
+	},
+	'8330': {
+		key: 'Skin_RDR',
+		name: 'Raider',
+		class: 'medium',
+		type: 'skin',
+		category: undefined
+	},
+	'8329': {
+		key: 'Skin_TCN',
+		name: 'Technician',
+		class: 'medium',
+		type: 'skin',
+		category: undefined
+	},
+	'8748': {
+		key: 'Skin_SLD_Mercenary',
+		name: 'Mercenary',
+		class: 'medium',
+		type: 'skin',
+		category: undefined
+	},
+	'8352': {
+		key: 'Skin_RDR_Mercenary',
+		name: 'Raider Mercenary',
+		class: 'medium',
+		type: 'skin',
+		category: undefined
+	},
+	'8731': {
+		key: 'Skin_TCN_Mercenary',
+		name: 'Technician Mercenary',
+		class: 'medium',
+		type: 'skin',
+		category: undefined
+	},
+	'8351': {
+		key: 'Skin_RDR_Griever',
+		name: 'Raider Griever',
+		class: 'medium',
+		type: 'skin',
+		category: undefined
+	}
 };
 
