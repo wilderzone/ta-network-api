92c469f2dff3352f3af7ab966f01f53e92a2add9
Giga <gigabyte5671@gmail.com>
2022-11-19 23:11:01 +1300

    Expose utility functions in the main package

--- index.ts
+++ index.ts
@@ -3,3 +3,4 @@ export { LoginServerConnection } from './functions/Main';
 export { Buffer } from './functions/Buffer';
 export { Decoder } from './functions/Decoder';
 export { GenericMessage, AuthenticationMessage, ServerListMessage, WatchNowMessage } from './functions/Messages';
+export { hexToString, stringToHex, textToHex, invertEndianness } from './functions/Utils';
