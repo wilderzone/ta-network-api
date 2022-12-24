4c37f5f81d30dcca7027739331809fbcd42f456e
Giga <gigabyte5671@gmail.com>
2022-12-24 13:37:05 +1300

    Update unit test runner

--- tests/index.ts
+++ tests/index.ts
@@ -1,15 +1,15 @@
-const { execSync } = require('child_process');
+import { execSync } from 'child_process';
 
 // Utility functions:
 
 console.log('Testing hexToString...');
-execSync('npx ts-node tests/hexToString.test.ts', {stdio: 'inherit'});
+execSync('node dist/tests/hexToString.test.js', {stdio: 'inherit'});
 
 console.log('Testing stringToHex...');
-execSync('npx ts-node tests/stringToHex.test.ts', {stdio: 'inherit'});
+execSync('node dist/tests/stringToHex.test.js', {stdio: 'inherit'});
 
 console.log('Testing textToHex...');
-execSync('npx ts-node tests/textToHex.test.ts', {stdio: 'inherit'});
+execSync('node dist/tests/textToHex.test.js', {stdio: 'inherit'});
 
 console.log('Testing invertEndianness...');
-execSync('npx ts-node tests/invertEndianness.test.ts', {stdio: 'inherit'});
+execSync('node dist/tests/invertEndianness.test.js', {stdio: 'inherit'});
