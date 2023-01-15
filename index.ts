e2f84af1914ca16ef540ccb6d46365278eef8555
Giga <gigabyte5671@gmail.com>
2023-01-16 12:33:18 +1300

    Export all Message classes under an alias

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ export * from './interfaces/index.js';
 export { LoginServerConnection } from './functions/Connections.js';
 export { Buffer } from './functions/Buffer.js';
 export { Decoder } from './functions/Decoder.js';
-export { GenericMessage, AuthenticationMessage, ServerListMessage, ServerInfoMessage, WatchNowMessage, DirectMessageMessage } from './functions/Messages.js';
+export * as Messages from './functions/Messages.js';
 export { hexToString, stringToHex, textToHex, invertEndianness } from './functions/Utils.js';
